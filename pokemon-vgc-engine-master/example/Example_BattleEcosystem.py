b9401b9102bd9e21e41e2a8d2b70f221c60e9125
GemmaRagadini <g.ragadini@studenti.unipi.it>
2025-01-02 16:31:33 +0100

    Modificato esempio di battaglia

--- pokemon-vgc-engine-master/example/Example_BattleEcosystem.py
+++ pokemon-vgc-engine-master/example/Example_BattleEcosystem.py
@@ -1,4 +1,4 @@
-from Example_Competitor import ExampleCompetitor
+from Example_Competitor import ExampleCompetitor, MyCompetitor
 from vgc.balance.meta import StandardMetaData
 from vgc.competition.Competitor import CompetitorManager
 from vgc.ecosystem.BattleEcosystem import BattleEcosystem
@@ -12,10 +12,16 @@ def main():
     roster = RandomPkmRosterGenerator().gen_roster()
     meta_data = StandardMetaData()
     le = BattleEcosystem(meta_data, debug=True)
-    for i in range(N_PLAYERS):
-        cm = CompetitorManager(ExampleCompetitor("Player %d" % i))
-        cm.team = RandomTeamFromRoster(roster).get_team()
-        le.register(cm)
+    cm1 = CompetitorManager(MyCompetitor("Player 0"))
+    cm1.team = RandomTeamFromRoster(roster).get_team()
+    le.register(cm1)
+    cm2 = CompetitorManager(ExampleCompetitor("Player 1"))
+    cm2.team = RandomTeamFromRoster(roster).get_team()
+    le.register(cm2)
+    # for i in range(N_PLAYERS):
+    #     cm = CompetitorManager(ExampleCompetitor("Player %d" % i))
+    #     cm.team = RandomTeamFromRoster(roster).get_team()
+    #     le.register(cm)
     le.run(10)
 
 
